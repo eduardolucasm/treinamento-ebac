1ca03ebcdc1a8a6cb9164f2eef4121aceade9e33
Eduardo Lucas <eduardo.alencar@puket.com.br>
2023-07-19 19:02:34 -0300

    Modulo 8 finalizado

---
+++ Modulo-8/script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById("form-calc");
+const imgAprovado = `<img src="images/aprovado.png" alt="Aprovado" />`;
+const imgReprovado = `<img src="images/reprovado.png" alt="Reprovado" />`;
+
+const atividades = [];
+const notas = [];
+const spanAprovado = `<span class="resultado aprovado">Aprovado</span>`;
+const spanReprovado = `<span class="resultado reprovado">Reprovado</span>`;
+const notaMinima = parseFloat(prompt("Digite a Nota Minima:"));
+
+let linhas = "";
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  adicionaLinha();
+  atualizaTabela();
+  atualizaMedia();
+});
+
+function adicionaLinha() {
+  const inputNome = document.getElementById("nome-atividade");
+  const inputNota = document.getElementById("nota-atividade");
+
+  if (atividades.includes(inputNome.value)) {
+    alert(`A atividade ${inputNome.value} foi inserida`);
+  } else {
+    atividades.push(inputNome.value);
+    notas.push(parseFloat(inputNota.value));
+
+    let linha = "<tr>";
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputNota.value}</td>`;
+    linha += `<td>${
+      inputNota.value >= notaMinima ? imgAprovado : imgReprovado
+    }</td>`;
+    // Colocamos ? para quando o resultado é positivo e : quando é negativo
+    linha += `</tr>`;
+
+    linhas += linha;
+  }
+  inputNome.value = "";
+  inputNota.value = "";
+}
+function atualizaTabela() {
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+}
+function atualizaMedia() {
+  const mediaFinal = calculaMedia();
+
+  document.getElementById("media-final").innerHTML = mediaFinal.toFixed(2);
+  document.getElementById("media-resultado").innerHTML =
+    mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+function calculaMedia() {
+  let somaNotas = 0;
+
+  for (let i = 0; i < notas.length; i++) {
+    somaNotas += notas[i];
+  }
+
+  return (media = somaNotas / notas.length);
+}
